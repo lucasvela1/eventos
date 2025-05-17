243ae9d89712859ec3c4aa82671fa8fdcab59b80
ibelinday <i_belinda_y@hotmail.com>
2025-05-16 21:22:18 -0300

    otra vez

--- eventos/settings.py
+++ eventos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-# holaaa comentario de Beli
+# de nuevo Beliii
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
