11491eba1ac718fd8809ecb37d9d31a413187d21
barby notebook <barbygs1900@gmail.com>
2025-05-21 15:56:42 -0300

    se agregaron casi todas las clases.

--- app/models.py
+++ app/models.py
@@ -1,12 +1,27 @@
 from django.db import models
-#Comentario de lucas Vela
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+class Venue(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    city = models.TextField()
+    capacity = models.IntegerField()
+    contact =models.TextField()
+
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    total_rating = models.IntegerField()
+    categoria = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    venue = models.OneToOneField(Venue, on_delete=models.SET_NULL, null=True, blank=False)
 
     def __str__(self):
         return self.title
@@ -44,3 +59,58 @@ class Event(models.Model):
         self.date = date or self.date
 
         self.save()
+
+class Priority(models.TextChoices):
+    high = 'HIGH'
+    medium = 'MEDIUM'
+    low = 'LOW'
+
+class Notification(models.Model):
+    title = models.CharField(max_length=200)
+    messaje = models.TextField()
+    created_at = models.DateField() 
+    priority = models.TextField(choices=Priority.choices)
+    read = models.BooleanField(default=False)
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.TextField()
+    notification = models.ForeignKey(Notification, on_delete=models.SET_NULL, null=True, blank=True )
+
+
+class RefundRequest(models.Model):
+    approved = models.BooleanField()
+    approval_date = models.DateField()
+    ticket_code = models.TextField(unique=True)
+    reason = models.TextField()
+    created_at = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False )
+
+class Type(models.TextChoices):
+    general = 'GENERAL'
+    vip = 'VIP'
+
+class Ticket(models.Model):
+    buy_date = models.DateField()
+    ticket_code = models.TextField(unique=True)
+    quantity = models.IntegerField()
+    type = models.TextField(choices=Type.choices, default=Type.general)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=False )
+    event = models.OneToOneField(Event, on_delete=models.CASCADE, null=True, blank=False )
+
+class Rating(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=False )
+    event = models.OneToOneField(Event, on_delete=models.CASCADE, null=True, blank=False )
+
+
+
+    
+
+
+
+
+    
