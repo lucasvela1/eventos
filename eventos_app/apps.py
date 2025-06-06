5b546956dc12e27080345ebc8aa6efddc862c7db
Lucas <lucas.tito1@hotmail.com>
2025-06-06 11:01:00 -0300

    Modificacion de carga inicial de modelos Se ejecuta ahora con el comando `python manage.py pueblaDatos` antes de runserver

--- eventos_app/apps.py
+++ eventos_app/apps.py
@@ -9,16 +9,4 @@ class AppConfig(AppConfig):
     ''' def ready(self):
         import eventos_app.signals  # Solo importa, no ejecuta nada aún
     '''
-
-    def ready(self): #Es parte de la inicializacion
-        # Evita que se ejecute dos veces con runserver
-        if threading.current_thread().name != "MainThread":
-            return #Sin esto se ejecuta dos veces
-
-        from django.core.management import call_command #Permite ejecutar comandos como si fuera la consola
-        try:
-            call_command('pueblaDatos') #Llama al script que poblara con los datos iniciales
-        except Exception as e:
-            print(f"Error cargando datos iniciales: {e}")
-
        
