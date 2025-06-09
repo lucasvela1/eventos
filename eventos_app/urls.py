8de9804c1c7e9a8ce0faf7a1905d2122cb6530aa
Garcia Barbara <barbygs1900@gmail.com>
2025-06-09 19:36:00 -0300

    Se modificaron botones de la navbar y el footer. Se comenzó a desarrollar la vista de "mi cuenta"

--- eventos_app/urls.py
+++ eventos_app/urls.py
@@ -6,11 +6,13 @@ from .views import (
     EventDetailView,
     NotificationListView,
     FavoritosListView,
+    ToggleFavoritoView,
     RefundRequestListView,
     RatingView,
     RegisterView,
     crear_rating,
-    BuscarEventosView
+    BuscarEventosView,
+    MiCuentaView
 )
 from . import views
 from .forms import LoginForm, UsuarioRegisterForm
@@ -22,10 +24,11 @@ urlpatterns = [
     path("notifications/", NotificationListView.as_view(), name="notifications"),
     path("refundRequests/", RefundRequestListView.as_view(), name="refundRequests"),
     path("favoritos/", FavoritosListView.as_view(), name="favoritos"),
+    path('eventos/<int:event_id>/toggle_favorito/', ToggleFavoritoView.as_view(), name='toggle_favorito'),
     path("rating/", RatingView.as_view(), name="rating"),
     path("events/<int:event_id>/calificar/", crear_rating, name='crear_rating'),
-    path('eventos/<int:event_id>/toggle_favorito/', views.toggle_favorito, name='toggle_favorito'),
     path('buscar/', BuscarEventosView.as_view(), name='buscar_eventos'),
     path("accounts/login/", auth_views.LoginView.as_view(template_name="accounts/login.html",authentication_form=LoginForm), name="login"),
     path("accounts/register/", RegisterView.as_view(), name="register"),
+    path("accounts/register/usuario/", MiCuentaView.as_view(), name="my_account" ),
 ]
